dd20c9be79cb306ef80de3c51972ca1fdf978df5
Tojo-rasii <tojorasii1619@gmail.com>
2024-04-17 10:19:50 +0300

    red

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
- // JavaScript to add 'active' class to the clicked navigation item
- document.addEventListener('DOMContentLoaded', function () {
-    var navLinks = document.querySelectorAll('header section article');
+document.addEventListener('DOMContentLoaded', function () {
+  var navLinks = document.querySelectorAll('header section article');
 
-    navLinks.forEach(function (link) {
-      link.addEventListener('click', function () {
-        navLinks.forEach(function (navLink) {
-          navLink.classList.remove('active');
-        });
-
-        link.classList.add('active');
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      // Remove 'active' class from all links
+      navLinks.forEach(function (navLink) {
+        navLink.classList.remove('active');
       });
+
+      // Add 'active' class to the clicked link
+      link.classList.add('active');
+
+      // Get the text content of the clicked link and display it
+      var text = link.querySelectorAll('header section article span:last-child').textContent;
+      document.querySelectorAll('.Title-Nav').textContent = text;
     });
   });
+
+  // Toggle dark mode
+  const darkModeButton = document.querySelector('.dark');
+  const darkModeButton2 = document.querySelector('.light');
+  const body = document.querySelector('body');
+
+  darkModeButton.addEventListener('click', function () {
+    body.classList.toggle('dark-mode');
+    darkModeButton.style.display = "none";
+    darkModeButton2.style.display = "block";
+  });
+
+  darkModeButton2.addEventListener('click', function () {
+    body.classList.toggle('dark-mode');
+    darkModeButton.style.display = "block";
+    darkModeButton2.style.display = "none";
+  });
+});
